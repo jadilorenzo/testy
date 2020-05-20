1f8677bbd0adccacdee9578ff2beb88bf86dce4a
Jacob Di Lorenzo <46056370+jadilorenzo@users.noreply.github.com>
2020-05-19 20:37:34 -0400

    Add Answer field

--- src/components/QuestionForm.tsx
+++ src/components/QuestionForm.tsx
@@ -5,6 +5,8 @@ export default (props: { type: string }) => {
   switch (props.type) {
     case 'multiple-choice':
       return <MultiOptionQuestionForm />
+    case 'multi-select':
+      return null
     default:
       return null
   }
